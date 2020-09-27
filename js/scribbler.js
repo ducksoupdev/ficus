48b6c15d7f08ccacaf4012aa8753ca06dfb0d3ef
ducksoupdev <ducksoupdev@gmail.com>
2020-09-27 20:07:36 +0000

    Deploying to gh-pages from  @ 0cf144f9ef09aafa8912d628c907416c7428ccbe 🚀

--- js/scribbler.js
+++ js/scribbler.js
@@ -1,1 +1,124 @@
-function get(t,e){return(e=e||document).querySelector(t)}function getAll(t,e){return(e=e||document).querySelectorAll(t)}const btns=getAll(".js-btn"),header1=get(".doc__content h1"),sections=getAll(".doc__content h2");function setActiveLink(t){for(let t=0;t<btns.length;t++)btns[t].classList.remove("active");t.target.parentNode.classList.add("active")}function smoothScrollTo(t,e){setActiveLink(e),window.scrollTo({behavior:"smooth",top:t.offsetTop-20,left:0})}if(header1&&sections.length>0){const t=Array.from(getAll(".doc__nav > ul > li")).find(t=>t.textContent===header1.textContent);if(t){const e=document.createElement("ul");Array.from(sections).forEach((function(t,o){const n=document.createElement("li");n.classList.add("js-btn"),0===o&&n.classList.add("active");const c=document.createElement("span");c.classList.add("doc__nav-item"),c.textContent=t.textContent,n.appendChild(c),e.appendChild(n),n.addEventListener("click",(function(e){smoothScrollTo(t,e)}))})),t.classList.add("selected"),t.appendChild(e)}}else if(header1&&0===sections.length){const t=Array.from(getAll(".doc__nav > ul > li")).find(t=>t.textContent===header1.textContent);t&&t.classList.add("selected")}const topNav=get(".menu"),icon=get(".toggle");window.addEventListener("load",(function(){icon.addEventListener("click",(function(){"menu"===topNav.className?(topNav.className+=" responsive",icon.className+=" open"):(topNav.className="menu",icon.classList.remove("open"))}))}));const backTop=get(".cd-top"),offset=300,offsetOpacity=1200;let scrolling=!1;function checkBackToTop(){const t=window.scrollY||document.documentElement.scrollTop;t>300?backTop.classList.add("cd-top--is-visible"):(backTop.classList.remove("cd-top--is-visible"),backTop.classList.remove("cd-top--fade-out")),t>1200&&backTop.classList.add("cd-top--fade-out"),scrolling=!1}backTop&&(window.addEventListener("scroll",(function(){scrolling||(scrolling=!0,window.requestAnimationFrame(checkBackToTop))})),backTop.addEventListener("click",(function(t){t.preventDefault(),window.scrollTo({behavior:"smooth",top:0,left:0})}))),["https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap"].forEach((function(t){const e=document.createElement("link");e.rel="stylesheet",e.href=t,e.type="text/css";const o=document.getElementsByTagName("link")[0];o.parentNode.insertBefore(e,o)}));
+function get (selector, scope) {
+  scope = scope || document
+  return scope.querySelector(selector)
+}
+
+function getAll (selector, scope) {
+  scope = scope || document
+  return scope.querySelectorAll(selector)
+}
+
+// in page scrolling for documentation page
+const btns = getAll('.js-btn')
+const header1 = get('.doc__content h1')
+const sections = getAll('.doc__content h2')
+
+function setActiveLink (event) {
+  // remove all active tab classes
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].classList.remove('active')
+  }
+
+  event.target.parentNode.classList.add('active')
+}
+
+function smoothScrollTo (element, event) {
+  setActiveLink(event)
+
+  window.scrollTo({
+    behavior: 'smooth',
+    top: element.offsetTop - 20,
+    left: 0
+  })
+}
+
+if (header1 && sections.length > 0) {
+  const docNav = Array.from(getAll('.doc__nav > ul > li'))
+  const header1NavItem = docNav.find(x => x.textContent === header1.textContent)
+  if (header1NavItem) {
+    const sectionList = document.createElement('ul')
+    Array.from(sections).forEach(function (sec, idx) {
+      const item = document.createElement('li')
+      item.classList.add('js-btn')
+      if (idx === 0) item.classList.add('active')
+      const span = document.createElement('span')
+      span.classList.add('doc__nav-item')
+      span.textContent = sec.textContent
+      item.appendChild(span)
+      sectionList.appendChild(item)
+      item.addEventListener('click', function (event) {
+        smoothScrollTo(sec, event)
+      })
+    })
+    header1NavItem.classList.add('selected')
+    header1NavItem.appendChild(sectionList)
+  }
+} else if (header1 && sections.length === 0) {
+  const docNav = Array.from(getAll('.doc__nav > ul > li'))
+  const header1NavItem = docNav.find(x => x.textContent === header1.textContent)
+  if (header1NavItem) {
+    header1NavItem.classList.add('selected')
+  }
+}
+
+// responsive navigation
+const topNav = get('.menu')
+const icon = get('.toggle')
+
+window.addEventListener('load', function () {
+  function showNav () {
+    if (topNav.className === 'menu') {
+      topNav.className += ' responsive'
+      icon.className += ' open'
+    } else {
+      topNav.className = 'menu'
+      icon.classList.remove('open')
+    }
+  }
+  icon.addEventListener('click', showNav)
+})
+
+const backTop = get('.cd-top')
+const offset = 300
+const offsetOpacity = 1200
+let scrolling = false
+
+if (backTop) {
+  window.addEventListener('scroll', function () {
+    if (!scrolling) {
+      scrolling = true
+      window.requestAnimationFrame(checkBackToTop)
+    }
+  })
+
+  // smooth scroll to top
+  backTop.addEventListener('click', function (event) {
+    event.preventDefault()
+    window.scrollTo({
+      behavior: 'smooth',
+      top: 0,
+      left: 0
+    })
+  })
+}
+
+function checkBackToTop () {
+  const windowTop = window.scrollY || document.documentElement.scrollTop;
+  if (windowTop > offset) {
+    backTop.classList.add('cd-top--is-visible')
+  } else {
+    backTop.classList.remove('cd-top--is-visible')
+    backTop.classList.remove('cd-top--fade-out')
+  }
+  (windowTop > offsetOpacity) && backTop.classList.add('cd-top--fade-out')
+  scrolling = false
+}
+
+['https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap'].forEach(function (f) {
+  const gf = document.createElement('link')
+  gf.rel = 'stylesheet'
+  gf.href = f
+  gf.type = 'text/css'
+  const gd = document.getElementsByTagName('link')[0]
+  gd.parentNode.insertBefore(gf, gd)
+})
